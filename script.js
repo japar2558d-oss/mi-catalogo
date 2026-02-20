5aae98c133f086ceae81fcaa2705d233a28499d1
japar2558d-oss <japar2558d@gmail.com>
2026-02-20 10:43:08 -0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+const apiKey = "b945301e275c564d8c5baf0f8c54d08d";
+
+const moviesDiv = document.getElementById("movies");
+const searchInput = document.getElementById("search");
+
+// 🔎 Buscar cuando escribas
+searchInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    searchMovies(searchInput.value);
+  }
+});
+
+async function searchMovies(query) {
+  if (!query) return;
+
+  moviesDiv.innerHTML = "Buscando...";
+
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=es-ES&query=${query}`
+  );
+
+  const data = await res.json();
+
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  moviesDiv.innerHTML = "";
+
+  if (movies.length === 0) {
+    moviesDiv.innerHTML = "No se encontraron resultados.";
+    return;
+  }
+
+  movies.forEach((movie) => {
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+      <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}">
+      <h3>${movie.title}</h3>
+      <p>⭐ ${movie.vote_average}</p>
+      <p>${movie.release_date || ""}</p>
+    `;
+
+    moviesDiv.appendChild(movieEl);
+  });
+}
